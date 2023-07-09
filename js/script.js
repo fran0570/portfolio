ff7e0988807805cb5c7e2a18cf09128cd3aafa3e
francois francoeur <fran0570@algonquinlive.com>
2023-07-08 20:51:46 -0400

    adds trial scoll coordinate function

--- js/script.js
+++ js/script.js
@@ -1,49 +1,35 @@
 // 3d parallax fx
 
-const skewItemContainer = document.querySelector(":root");
-const skewItem = document.querySelector("#promo-item-wrap").getBoundingClientRect();
+const skewItemContainer = document.querySelector(':root');
+const skewItem = document.querySelector('#promo-item-wrap').getBoundingClientRect();
 const imageCenterX = skewItem.left + skewItem.width / 2;
 const imageCenterY = skewItem.top + skewItem.height / 2;
 
 // for mouse move
-skewItemContainer.addEventListener("mousemove", function (e) {
+skewItemContainer.addEventListener('mousemove', function (e) {
   const clientX = e.clientX;
   const clientY = e.clientY;
   const xCalculation = (clientX - imageCenterX) * 0.01;
   const yCalculation = (clientY - imageCenterY) * 0.01;
 
-  skewItemContainer.style.setProperty("--x-translate", `${xCalculation}`);
-  skewItemContainer.style.setProperty("--y-translate", `${yCalculation}`);
-  console.log(xCalculation, yCalculation);
+  skewItemContainer.style.setProperty('--x-translate', `${xCalculation}`);
+  skewItemContainer.style.setProperty('--y-translate', `${yCalculation}`);
+  // console.log(xCalculation, yCalculation);
+  const yPos =  document.documentElement.scrollTop;
+  console.log('here is my third attempt', yPos);
 });
 
+document.getElementById('uView').addEventListener('click', printLocation);
 
+// alway returns 0 because the data is recorder on page load only no update loop
+function printLocation() {
+  // const viewLocation = document.querySelector('.user-view').getBoundingClientRect();
+  // console.log('here is the view location coordinate: ', viewLocation);
+  // const yPos =  document.documentElement.scrollTop;
+  // console.log('here is my third attempt', yPos);
+}
 
-const skewItemContainer2 = document.querySelector(":root");
-const skewItem2 = document.querySelector("#item-poster").getBoundingClientRect();
-const imageCenterX2 = skewItem2.left + skewItem2.width / 2;
-const imageCenterY2 = skewItem2.top + skewItem2.height / 2;
-
-// for mouse move
-skewItemContainer2.addEventListener("mousemove", function (e) {
-  const clientX2 = e.clientX;
-  const clientY2 = e.clientY;
-  const xCalculation2 = (clientX2 - imageCenterX2) * 0.01;
-  const yCalculation2 = (clientY2 - imageCenterY2) * 0.01;
-
-  skewItemContainer2.style.setProperty("--x-translate", `${xCalculation2}`);
-  skewItemContainer2.style.setProperty("--y-translate", `${yCalculation2}`);
-  console.log(xCalculation2, yCalculation2);
-});
-
-// for scrolling or touche
-// skewItemContainer.addEventListener("ontouchmove", function (e) {
-//   const clientX = e.clientX;
-//   const clientY = e.clientY;
-//   const xCalculation = (clientX - imageCenterX) * 0.01;
-//   const yCalculation = (clientY - imageCenterY) * 0.01;
-
-//   skewItemContainer.style.setProperty("--x-translate", `${xCalculation}`);
-//   skewItemContainer.style.setProperty("--y-translate", `${yCalculation}`);
-//   console.log(xCalculation, yCalculation);
-// });
+if (window.scrollY > 400) {
+  window.scroll(0, 0);
+}
+// document.documentElement.scrollTop || document.body.scrollTop
